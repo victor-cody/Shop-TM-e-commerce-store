32fd8f1d0acd09b939be7cf21d77e16b20ff7c03
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2023-07-10 11:36:18 +0100

    add ProductCard and ProductsGrid components

---
+++ components/ProductCard.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="relative px-6 py-2">
+		<NuxtLink
+			:to="`products/${slug}`"
+			v-editable="product"
+			class="overflow-hidden w-full cursor-pointer py-5"
+		>
+			<img
+				v-if="product?.product_images.length > 0"
+				:src="product?.product_images[0].filename"
+				:alt="product.product_images[0].alt"
+				class="w-full max-h-[34.5rem] mb-5 object-cover object-top shadow-sm hover:shadow-md hover:scale-[1.02] transition-all duration-300 ease-in-out"
+			/>
+			<span
+				class="absolute z-10 top-[1.2%] right-[5.6%] px-4 py-4 max-w-[30%] text-sm font-thin text-white bg-black flex justify-center items-center"
+				>{{ product.in_stock ? "For sale" : "Out of sale" }}</span
+			>
+		</NuxtLink>
+		<div
+			class="flex flex-col items-start justify-around text-lg text-[#383838]"
+		>
+			<h3 class="mb-0.5 uppercase py-1">
+				{{ product.name }}
+			</h3>
+			<span class="text-[#777] tracking-wider">
+				$&nbsp;{{ product.price }}
+			</span>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	defineProps({ product: Object, slug: String });
+</script>
---
+++ storyblok/ProductsGrid.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="container mx-auto font-light" v-editable="blok">
+		<h2
+			className="font-normal tracking-tighter leading-tight text-4xl lg:text-6xl mb-7 lg:mb-12"
+		>
+			{{ blok.headline }}
+		</h2>
+		<div
+			className="w-full grid grid-flow-row gap-x-6 lg:gap-y-1.5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-6 lg:mb-8"
+		>
+			<ProductCard
+				v-for="product in blok.products"
+				:product="product.content"
+				:slug="product.slug"
+				:key="product.uuid"
+				class="my-6"
+			/>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	defineProps({ blok: Object });
+</script>
